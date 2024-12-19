3707afce796ecfc61a38afa0ecd393b375be8543
unknown <oleff@mail.ru>
2024-12-20 00:49:33 +0300

    conf file

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const mysql = require('mysql2/promise');
+const env = process.env.NODE_ENV || 'dbmysql';
 
 const { sendEmailApi } = require('./send_email.js');
 
 const app=express();
 
-//const cityMail='spb'
+const { config } = require('./config.js');
+const mysqlConfig = config[env];
+
+const cityMail='spb'
 //const cityMail='Москва'
 //const cityMail='Московская область'
 //const cityMail='Новгородская область'
@@ -16,7 +20,7 @@ const app=express();
 //const cityMail='Кемеровская область'
 //const cityMail='Нижегородская область'
 //const cityMail='Башкортастан республика'
-const cityMail='Красноярский край'
+//const cityMail='Красноярский край'
 
 
     var all_email;
@@ -26,29 +30,41 @@ const cityMail='Красноярский край'
         return new Promise(resolve => setTimeout(resolve, 10000));
     }
 
-    async function delayedSendEmail(mailSend, clientId) {
+    async function delayedSendEmail(mailSend, clientId,taskId) {
         // мы можем использовать await для Promise
         // который возвращается из delay
 
         await delay();
-        await sendEmailApi(mailSend, clientId)
+        let resultMailSend=await sendEmailApi(mailSend, clientId,taskId)
 
-        const con_cloud = await mysql.createConnection({host: "37.140.195.179", user: "weblife", password: '30890863089086', database: "wl_crm"});
+        const con_cloud = await mysql.createConnection({
+            ...mysqlConfig,
+        });
 
-        const $query="UPDATE `base_medicals_companies` SET `is_sended`=1 WHERE `id`='"+clientId+"'";
-        con_cloud.execute($query);
+        if(resultMailSend){
+            const $query="UPDATE `wl_mailer_tasks_items` SET `is_sended`=1 WHERE `id`='"+clientId+"'";
+            con_cloud.execute($query);
 
-        console.log(mailSend+' : '+clientId);
+            console.log(mailSend+' : '+clientId);
+        }
+        else{
+            
+            const $query="UPDATE `wl_mailer_tasks_items` SET `error`="+resultMailSend+" WHERE `id`='"+clientId+"'";
+            con_cloud.execute($query);
+            console.log('Error, mail no send: '+mailSend+' : '+clientId);
+        }        
     }
       
 
       async function getAllEmail(req, res){
 
-        console.log('!!!')
+        //console.log('!!!')
         
-        const con_cloud = await mysql.createConnection({host: "37.140.195.179", user: "weblife", password: '30890863089086', database: "wl_crm"});
+        const con_cloud = await mysql.createConnection({
+            ...mysqlConfig,
+        });
         
-        const $select="SELECT * FROM `base_medicals_companies` where email is not null and is_sended!=1 and  `city` LIKE '"+cityMail+"' AND `type` LIKE 'Диагностические центры' limit 50"; //5126
+        const $select="SELECT * FROM `wl_mailer_tasks_items` where email is not null and is_sended!=1 and  `city` LIKE '"+cityMail+"' limit 50"; //5126
         [all_email, fields3] = await con_cloud.execute($select);
 
         
@@ -57,13 +73,15 @@ const cityMail='Красноярский край'
             let email=client.email;
             let email_arr=email.split(',')
             let clientId=client.id;            
+            let taskId=client.task_id;
             
             for(const mail of email_arr){
 
                 let mailSend=mail; 
                 ListEmailSended+=mailSend+'; '       
 
-                await delayedSendEmail(mailSend, clientId)
+                await delayedSendEmail(mailSend, clientId, taskId)
+                //console.log(mailSend);
             }            
         }
         
@@ -78,7 +96,7 @@ const cityMail='Красноярский край'
       app.get('/sendmail', getAllEmail)
      
       
-app.listen(4000,(err)=> {
+app.listen(30000,(err)=> {
     if(err){
         return console.log(err);
     }
--- send_email.js
+++ send_email.js
@@ -1,26 +1,23 @@
-
+const {config} = require('./config.js');
 const nodemailer = require('nodemailer');
+
+const env = process.env.NODE_ENV || 'configMail';
+
+const mailConfig = config[env];
 
 //let clientId
 //let mailSend
 
-async function sendEmailApi(email,clientId){
+async function sendEmailApi(email,clientId,taskId){
     //let testEmailAccount = await nodemailer.createTestAccount();
 
+    console.log(mailConfig)
+    
     let transporter = nodemailer.createTransport({
-        host: 'mail.ru-weblife.ru',
-        port: 25,
-        secure: false,
-        auth: {
-            user: 'zakaz@ru-weblife.ru',
-            pass: 'jJ4xL1vI6p',
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
+        ...mailConfig,
     });
 
-    emailSubject='Программное обеспечение для медицинских и диагностических центров, хранение и просмотра исследований через любой веб браузер.';
+    emailSubject='Отчет по кэшбэку';
     let emailHTML=`
     <!doctype html>
 <html lang="ru">
@@ -31,26 +28,15 @@ async function sendEmailApi(email,clientId){
 <body>
  
     <div style="text-align: center;">
-        <h2>Комплексное программное обеспечение для медицинских и диагностических организаций.<br> 
-            Ведения учета, хранение и просмотр исследований через любой веб браузер из любой ОС.</h2>
+        <h2>Отчет по кэшбэку</h2>
     </div>
     <div style="text-align: center; background-color:aliceblue;">
 
             <h3>Возможности:</h3>
 
             <div style="width: 340px; margin: 0 auto; text-align: left; padding-bottom: 20px;">              
-              <ul>
-                <li>Облако хранения снимков</li>
-                <li>Просмотр снимков(КТ и др.) из любого Веб-броузера</li>
-                <li>ЦРМ для для всех сотрудников компании</li>
-                <li>В ЦРМ есть города и центры для сетевых компаний</li>
-                <li>Личный кабинет для клиентов и партнеров</li>
-                <li>Специализированное ПО для работы со снимками</li>
-              </ul>
+              
             </div>
-      </div>
-      <div style="text-align: center;">
-            <img width="400" src="https://ru-weblife.ru/wp-content/uploads/2024/07/diag_001.jpg" >
       </div>
       <div style="text-align: center; background-color:aliceblue; padding-bottom: 20px;;">
             <div style="width: 480px; margin: 0 auto; ">
@@ -59,7 +45,7 @@ async function sendEmailApi(email,clientId){
                 Описание, видео, запрос на доступ к демо версии:
               </div>
               <br>
-              <a href="https://ru-weblife.ru/diagcenter/?wl_pixel_id=${clientId}" target="_blank" style="display: inline-block; cursor: pointer;font-size: 22px;
+              <a href="#" target="_blank" style="display: inline-block; cursor: pointer;font-size: 22px;
               color: white;
               text-decoration: none;
               padding: .5em 2em;
@@ -73,8 +59,7 @@ async function sendEmailApi(email,clientId){
             </div>
             <br>
             <br>
-            <img src="https://ru-weblife.ru/price001.jpg" >
-            <img width="1" height="1" src="https://ru-weblife.ru/pixel01.php?wl_pixel_id=${clientId}" >
+            <img width="1" height="1" src="https://crm.waldorf-xray.ru/pixel01.php?wl_pixel_id=${clientId}&trackn=${taskId}" >
             
 
     </div>
@@ -87,31 +72,25 @@ async function sendEmailApi(email,clientId){
 
 
 let emailText=`
-    Комплексное программное обеспечение для медицинских и диагностических организаций.<br> 
-            Ведения учета, хранение и просмотр исследований через любой веб браузер из любой ОС\n\n 
+    Отчет по кэшбэку\n\n 
 
-            Возможности:\n
-            Облако хранения снимков
-                - Просмотр снимков(КТ и др.) из любого Веб-броузера\n
-                - ЦРМ для для всех сотрудников компании\n
-                - В ЦРМ есть города и центры для сетевых компаний\n
-                - Личный кабинет для клиентов и партнеров</li>\n
-                - Специализированное ПО для работы со снимками\n\n
-                Описание, видео, запрос на доступ к демо версии:\n
-                https://ru-weblife.ru/diagcenter/`
+    Скачать:\n
+                https://crm.waldorf-xray.ru/`
 
                 //console.log('!!!');
     try {
         let result = await transporter.sendMail({
-            from: '"Веб Лайф" <zakaz@ru-weblife.ru>',
+            from: '"ООО Вальдорф" <zakaz@ru-weblife.ru>',
             to: email,
             subject: emailSubject,
             text: emailText,
             html: emailHTML,
         });    
+        return true;
     }
      catch (error) {
       console.log(error);
+      return error;
     }
     
     //console.log(result);
